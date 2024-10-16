fd70598a81510f89f0fd1a45f4a16d76e24c3d70
devrobertom <robertomaia2006@gmail.com>
2024-10-16 16:09:49 -0300

    atualização no css

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var inputAdd = document.getElementById("addItem")
+var btn = document.getElementById("btn")
+var res = document.getElementById("res")
+
+btn.addEventListener("click", adicionar)
+
+function adicionar(){
+    res.innerHTML = "oi"
+}
