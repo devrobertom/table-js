d2ee6928baa411044255a318dfdd3517c8094075
devrobertom <robertomaia2006@gmail.com>
2024-10-16 16:38:21 -0300

    Finalização do projeto

--- scripts.js
+++ scripts.js
@@ -1,9 +1,18 @@
 var inputAdd = document.getElementById("addItem")
 var btn = document.getElementById("btn")
-var res = document.getElementById("res")
+var res = document.getElementById("item")
+var remove = document.getElementById("remove")
 
 btn.addEventListener("click", adicionar)
 
 function adicionar(){
-    res.innerHTML = "oi"
+    var newitem = (inputAdd.value)
+    
+    res.innerHTML += `${newitem} </br>`
+}
+
+remove.addEventListener("click", apagar)
+
+function apagar(){
+    res.innerHTML = null
 }
